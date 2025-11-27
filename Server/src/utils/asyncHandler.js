a9499ddfa907aa717b565db49dcd75ad02aff006
shubhzVidhate <shubhamvidhate302@gmail.com>
2025-11-27 22:03:27 +0530

    utils code discusstion create ApiError,ApiResponce And AsyncHandle template

---
+++ Server/src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// ASYNCHANDLER USING PROMISES
+
+const asyncHandler = (requestHandler) => {
+  async (req, res, next) => {
+    Promise
+    .resolve(requestHandler(req, res, next))
+    .catch((err) => {
+      next(err);
+    });
+  };
+};
+
+
+export {asyncHandler};
+
+
+// ASYNCHANDLER USING TRYCATCH BLOCK
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next());
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
